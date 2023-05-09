d0bc4976ebcc71b62cab45debe4a80b368a9a12d
tiborkoderman <tibor.koderman@gmail.com>
2023-05-09 21:47:46 +0200

    first commit

---
+++ src/HTB.java
@@ -0,0 +1,13 @@
+//ignore duplicates
+//integers
+public class HTB {
+    //p = množitelj
+    //m = velikost tabele
+    //c1 = konstanta 1
+    //c2 = konstanta 2
+
+    //uporaba funkcije h'(k, i) = (h(k) + c1 * i + c2 * i2) mod m
+    //Ko je to potrebno (i == m), razširimo tabelo na novo velikost: m ← 2 * m + 1
+    HTB(int p, int m, int c1, int c2) {
+    }
+}
